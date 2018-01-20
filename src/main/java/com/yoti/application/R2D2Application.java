42191a5d346dc7335b96b6f2d2857053dfb8fb74
Chronic <chronic6666@gmail.com>
2018-01-20 20:55:34 +0100

    Converting DTO into work model

--- src/main/java/com/yoti/application/R2D2Application.java
+++ src/main/java/com/yoti/application/R2D2Application.java
@@ -1,7 +1,23 @@
 package com.yoti.application;
 
+import com.yoti.application.control.CleaningService;
+import com.yoti.application.converter.InputConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.support.DefaultConversionService;
 
 @SpringBootApplication
 public class R2D2Application {
+
+    @Autowired
+    private InputConverter inputConverter;
+
+    @Bean
+    public ConversionService conversionService() {
+        final DefaultConversionService cs = new DefaultConversionService();
+        cs.addConverter(inputConverter);
+        return cs;
+    }
 }
--- src/main/java/com/yoti/application/entity/RoomInput.java
+++ src/main/java/com/yoti/application/entity/RoomInput.java
@@ -41,35 +41,26 @@ public class RoomInput {
     }
 
     public static class RoomInputBuilder {
-        private RoomInputBuilder instance;
         private Room roomToBuild;
         private Coords botCoordsToBuild;
         private List<Instruction> instructionsToBuild;
+
         public RoomInputBuilder withRoom(Room room) {
-            init();
             this.roomToBuild = room;
-            return instance;
+            return this;
         }
         public RoomInputBuilder withBotAt(Coords coords) {
-            init();
             this.botCoordsToBuild = coords;
-            return instance;
+            return this;
         }
         public RoomInputBuilder withInstructions(List<Instruction> instructions) {
-            init();
             this.instructionsToBuild = instructions;
-            return instance;
+            return this;
         }
         public RoomInput build() {
             if(roomToBuild == null || botCoordsToBuild == null || instructionsToBuild == null || instructionsToBuild.isEmpty())
                 throw new RuntimeException("RoomBuilder was not given enough information to build a room");
             return new RoomInput(roomToBuild, botCoordsToBuild, new LinkedList<>(instructionsToBuild));
-        }
-
-        private void init() {
-            if (this.instance == null) {
-                this.instance = new RoomInputBuilder();
-            }
         }
 
     }
